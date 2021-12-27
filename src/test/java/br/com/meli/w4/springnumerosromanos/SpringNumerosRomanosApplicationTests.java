048d2b234c08b861de48e56facdefaca58ce6cf6
vfeitoza <vinicius.feitoza@mercadolivre.com>
2021-12-27 20:02:32 -0300

    Adiciona conversor para algarismo romano

---
+++ src/test/java/br/com/meli/w4/springnumerosromanos/SpringNumerosRomanosApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.meli.w4.springnumerosromanos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringNumerosRomanosApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
