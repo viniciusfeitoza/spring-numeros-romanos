dc8a635e530d2536089837e3f6df25f63e8f5263
vfeitoza <vinicius.feitoza@mercadolivre.com>
2021-12-27 20:01:07 -0300

    Adiciona conversor para algarismo romano

---
+++ src/main/java/br/com/meli/w4/springnumerosromanos/controller/MyController.java
@@ -0,0 +1,32 @@
+package br.com.meli.w4.springnumerosromanos.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MyController {
+
+    @GetMapping("/ping")
+    public String meuMetodo(){
+        return "pong";
+    }
+
+    @GetMapping("/{n}")
+    public String numRomano(@PathVariable String n){
+
+        return "I".repeat(Integer.parseInt(n))
+                .replace("IIIII", "V")
+                .replace("IIII", "IV")
+                .replace("VV", "X")
+                .replace("VIV", "IX")
+                .replace("XXXXX", "L")
+                .replace("XXXX", "XL")
+                .replace("LL", "C")
+                .replace("LXL", "XC")
+                .replace("CCCCC", "D")
+                .replace("CCCC", "CD")
+                .replace("DD", "M")
+                .replace("DCD", "CM");
+    }
+}
